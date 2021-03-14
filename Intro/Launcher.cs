29b963148934b76c9fac85d5e9f606a99b024fbd
Nadiia Stasyk <nadiia.stasyk@orbussoftware.com>
2021-03-14 12:28:20 +0000

    replaced StartUp with Launcher

--- Intro/Launcher.cs
+++ Intro/Launcher.cs
@@ -1,21 +1,17 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Intro
 {
-    public class Startup
+    public class Launcher
     {
         private readonly IConfiguration _configuration;
 
-        public Startup(IConfiguration configuration)
+        public Launcher(IConfiguration configuration)
         {
             _configuration = configuration;
         }
