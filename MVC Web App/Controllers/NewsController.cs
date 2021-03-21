48c4a256bc7e87c26dbd1e9162ed200edf64acef
Nadiia Stasyk <nadiia.stasyk@orbussoftware.com>
2021-03-21 16:56:25 +0000

    Added new project with News page

---
+++ MVC Web App/Controllers/NewsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_Web_App.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MVC_Web_App.Controllers
+{
+    public class NewsController : Controller
+    {
+        public IActionResult Index()
+        {
+            IEnumerable<News> allNews = NewsBase.GetNews();
+            return View(allNews);
+        }
+
+        public IActionResult Get(int index)
+        {
+            var news = NewsBase.GetNews();
+            ViewData["News"] = news.SingleOrDefault(x => x.Id == index);
+
+            return View();
+        }
+    }
+}
---
+++ MVC Web App/Models/News.cs
@@ -0,0 +1,15 @@
+﻿namespace MVC_Web_App.Models
+{
+    public class News
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Text { get; set; }
+
+        public string AuthorName { get; set; }
+
+        public bool IsFake { get; set; }
+    }
+}
